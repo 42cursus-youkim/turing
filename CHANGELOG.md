efea4e97e81333b0d64106e65361d6c9afe986b1
scarf <greenscarf005@gmail.com>
2022-09-19 11:20:40 +0900

    feat: JSON Schema (#1)
    * feat: JSON Schema generation script
    
    * feat: auto JSON path generation
    
    * build: first compilation

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for turing
+
+## 0.1.0.0 -- 2022-09-19
+
+* First version. Added json schema to validate program files.
---
+++ util/model.py
@@ -0,0 +1,56 @@
+#!/bin/env python3
+
+
+from pydantic import BaseModel, ConstrainedStr, Field, conlist
+
+from util.utils import print_diff, save_schema
+
+
+class Char(ConstrainedStr):
+    min_length = 1
+    max_length = 1
+
+
+class NonEmptyStr(ConstrainedStr):
+    min_length = 1
+
+
+Chars = conlist(Char, min_items=1)
+
+
+class Action(BaseModel):
+    read: Char
+    to_state: NonEmptyStr
+    write: Char
+    action: NonEmptyStr
+
+
+class Program(BaseModel):
+    name: NonEmptyStr = Field(..., description="Name of program")
+
+    alphabet: Chars = Field(..., description="Program symbols of the program")
+    blank: Char = Field(..., description="Symbol to render as blank tape during execution")
+
+    states: frozenset[NonEmptyStr]
+    initial: NonEmptyStr
+    finals: frozenset[NonEmptyStr]
+
+    transitions: dict[NonEmptyStr, list[Action]]
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+
+    settings = Path(".vscode/settings.json")
+    schema = Path(".vscode/turing.schema.json").absolute()
+
+    text = Program.schema_json(indent=2)
+
+    if not schema.exists():
+        print(text)
+        exit(0)
+
+    if (org_text := schema.read_text()) != text:
+        print_diff(org_text, text)
+
+    save_schema(settings, schema, text)
