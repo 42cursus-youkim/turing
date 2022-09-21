092de8c7c0e041dc6dad1dc0698661cd837840a2
scarf <greenscarf005@gmail.com>
2022-09-21 10:41:11 +0900

    refactor: a bit of formatting

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,33 @@
 module Main where
 
+import qualified Data.Map as Map
 import PyF (fmt)
 
+data Action = Action
+  { read :: Char,
+    to_state :: String,
+    write :: Char,
+    action :: String
+  }
+  deriving (Eq, Show)
+
+data Program = Program
+  { name :: String,
+    alphabet :: [Char],
+    blank :: Char,
+    states :: [String],
+    initial :: String,
+    final :: [String],
+    transitions :: Map.Map String Action
+  }
+  deriving (Show)
+
 main :: IO ()
-main = putStrLn [fmt|Hello, {name}!!|]
+main =
+  putStrLn [fmt|{hello}, {world}|]
   where
-    name = "World"
+    hello = "Hello"
+    world = "World"
+
+-- where
+-- act = Action {read = 'a', to_state = "q1", write = 'b', action = "R"}
