2ab4c6f3566aa51592098cc0d5ecc38a5f2f4acc
scarf <greenscarf005@gmail.com>
2022-09-26 14:43:29 +0900

    refactor: tidy up machine and action

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,7 @@ import Machine.Machine (Machine (..), initMachine, pprintMachine, runMachine)
 import Machine.Tape (pfTape)
 import Model.Program (pprintProgram)
 import Model.Reader (readProgram, testFile)
+import Util (termWidth)
 
 utm :: String
 utm = ">010101100111$00-01|00-00-00-00-1|00-01-00-01-1|00-10-00-10-1|00-11-01-00-0|01-01-10-00-0|01-10-11-00-0|10-01-10-01-0|10-10-11-01-0$"
@@ -18,8 +19,11 @@ runFrom file input = do
     Left e -> print e
     Right p -> do
       pprintProgram p
-      runMachine (initMachine input p)
+      let m = initMachine input p
+      runMachine m
 
 main :: IO ()
-main = runFrom "docs/examples/utm.json" utm
-  -- runFrom "docs/examples/unary_add.json" "111+11="
+main =
+  runFrom "docs/examples/utm.json" utm
+
+-- runFrom "docs/examples/unary_add.json" "111+11="
